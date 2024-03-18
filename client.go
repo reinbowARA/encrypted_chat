ea5efee24079f250ab5ff707f48839333ed0c862
Flisthdo <zaginyko@gmail.com>
2024-03-18 14:08:18 +0300

    start point

---
+++ client.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/tls"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/quic-go/quic-go"
+)
+
+func main() {
+
+	hostName := flag.String("hostname", "localhost", "hostname/ip of the server")
+	portNum := flag.String("port", "4242", "port number of the server")
+	//numEcho := flag.Int("necho", 100, "number of echos")
+	timeoutDuration := flag.Int("rtt", 1000, "timeout duration (in ms)")
+
+	flag.Parse()
+
+	addr := *hostName + ":" + *portNum
+
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo"},
+	}
+
+	session, err := quic.DialAddr(context.Background(), addr, tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	timeout := time.Duration(*timeoutDuration) * time.Millisecond
+
+	resp := make(chan string)
+
+	for {
+		message, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		log.Printf("Client: Sending '%s'", message)
+		_, err = stream.Write([]byte(message))
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("Done. Waiting for echo")
+
+		go func() {
+			buff := make([]byte, len(message))
+			_, err = io.ReadFull(stream, buff)
+			if err != nil {
+				panic(err)
+			}
+
+			resp <- string(buff)
+		}()
+
+		select {
+		case reply := <-resp:
+			log.Printf("Client: Got '%s'", reply)
+		case <-time.After(timeout):
+			log.Printf("Client: Timed out\n")
+		}
+
+		/*if counter == *numEcho {
+			break
+		}*/
+	}
+}
---
+++ server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"io"
+	"log"
+	"math/big"
+
+	"github.com/quic-go/quic-go"
+)
+
+func main() {
+	hostName := flag.String("hostname", "localhost", "hostname/ip of the server")
+	portNum := flag.String("port", "4242", "port number of the server")
+
+	flag.Parse()
+
+	addr := *hostName + ":" + *portNum
+
+	log.Println("Server running @", addr)
+
+	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+	if err != nil {
+		log.Println(err)
+	}
+	sess, err := listener.Accept(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+
+	for {
+		stream, err := sess.AcceptStream(context.Background())
+		if err != nil {
+			log.Println(err)
+		}
+
+		// Echo through the loggingWriter
+		_, err = io.Copy(loggingWriter{stream}, stream)
+	}
+}
+
+// A wrapper for io.Writer that also logs the message.
+type loggingWriter struct{ io.Writer }
+
+func (w loggingWriter) Write(b []byte) (int, error) {
+	msg := string(b)
+
+	log.Printf("Server: Got '%s'", msg)
+	log.Printf("Server: Sending '%s'", msg)
+
+	return w.Writer.Write(b)
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		log.Println(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		log.Println(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		log.Println(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo"},
+	}
+}
